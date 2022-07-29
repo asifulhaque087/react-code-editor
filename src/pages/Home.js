f75d6ae1be7744c3eef5d2379a1156248a433701
asiful <asiful@pop-os.localdomain>
2022-07-29 10:33:45 +0600

    add Home component html

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,43 @@
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img
+          className="homePageLogo"
+          src="/code-sync.png"
+          alt="code-sync-logo"
+        />
+        <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+        <div className="inputGroup">
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="ROOM ID"
+          />
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="USERNAME"
+          />
+          <button className="btn joinBtn">
+            Join
+          </button>
+          <span className="createInfo">
+            If you don't have an invite then create &nbsp;
+            <a  href="" className="createNewBtn">
+              new room
+            </a>
+          </span>
+        </div>
+      </div>
+      <footer>
+        <h4>
+          Built with 💛 &nbsp; by &nbsp;
+          <a href="https://github.com/asifulhaque087">Asiful Haque</a>
+        </h4>
+      </footer>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
