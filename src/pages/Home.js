a1563728abeeacfc461409a923222606cc16dfc5
asiful <asiful@pop-os.localdomain>
2022-07-29 10:48:24 +0600

    generate unique id and bind form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,15 @@
+import React, { useState } from "react";
+import { v4 as uuidV4 } from "uuid";
+
 const Home = () => {
+  const [roomId, setRoomId] = useState("");
+  const [username, setUsername] = useState("");
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidV4();
+    setRoomId(id);
+  };
+
   return (
     <div className="homePageWrapper">
       <div className="formWrapper">
@@ -13,18 +24,20 @@ const Home = () => {
             type="text"
             className="inputBox"
             placeholder="ROOM ID"
+            onChange={(e) => setRoomId(e.target.value)}
+            value={roomId}
           />
           <input
             type="text"
             className="inputBox"
             placeholder="USERNAME"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
           />
-          <button className="btn joinBtn">
-            Join
-          </button>
+          <button className="btn joinBtn">Join</button>
           <span className="createInfo">
             If you don't have an invite then create &nbsp;
-            <a  href="" className="createNewBtn">
+            <a href="" onClick={createNewRoom} className="createNewBtn">
               new room
             </a>
           </span>
