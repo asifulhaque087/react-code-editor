fb11386ce2e956929271e3b68c1537695484a6b4
asiful <asiful@pop-os.localdomain>
2022-07-29 10:55:09 +0600

    add react-hot-toast

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import EditorPage from './pages/EditorPage';
-import Home from './pages/Home';
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import EditorPage from "./pages/EditorPage";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
+      <div>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            success: {
+              theme: {
+                primary: "#4aed88",
+              },
+            },
+          }}
+        ></Toaster>
+      </div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/editor/:roomId" element={<EditorPage/>}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/editor/:roomId" element={<EditorPage />}></Route>
         </Routes>
       </BrowserRouter>
     </>
